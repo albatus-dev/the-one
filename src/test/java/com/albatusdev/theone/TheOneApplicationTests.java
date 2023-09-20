76a3338b2d6192b681defb9d5d2ff79b16725d56
albatus-dev <albatusdev@gmail.com>
2023-09-20 11:15:19 +0200

    init

---
+++ src/test/java/com/albatusdev/theone/TheOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albatusdev.theone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
